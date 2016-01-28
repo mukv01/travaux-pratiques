78bb8974cb20e3eee90901c93bd8f7ff95364e00
mukv01 <mukv01@uqo.ca>
2016-01-27 21:57:43 -0500

    Boutons ajoutes

--- projet1/courriel.js
+++ projet1/courriel.js
@@ -1,19 +1,32 @@
-etat = {
+
+var etat = {
   "inbox": [
     {
       "from": "AF22111212232211122",
-      "date": "2015 12 28 20:15:42"
+      "date": "2015 12 28 20:15:42",
       "msg": "Un court message ...." },
     {
       "from": "AF22111212232211122",
-      "date": "2016 01 03 10:15:31"
+      "date": "2016 01 03 10:15:31",
       "msg": "Un autre message ...." } ],
   "outbox": [
     {
       "to": "AF22111212232211122",
-      "date": "2016 01 12 20:15:42"
+      "date": "2016 01 12 20:15:42",
       "msg": "Bla bla bla ...." } ],
   "yp": {
     "AF22111212232211122": {"name": "Jean Fanchon"},
     "90221F212A4200001AA": {"name": "Bob"} }
+};
+
+function onButtonInboxClick(){
+  alert("Inbox");
+}
+
+function onButtonOutboxClick(){
+  alert("outbox");
+}
+
+function onButtonYpClick(){
+  alert("Yp");
 }
